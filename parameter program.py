8a5d000833f08901ff03110485960c30cc9d31f8
sayyedsy <55105451+sayyedsy@users.noreply.github.com>
2019-09-18 00:39:56 +0530

    Add files via upload

---
+++ parameter program.py
@@ -0,0 +1,6 @@
+#keyword and default parameter
+#formal and parameter
+#at the time of function call its actual parameter
+def person(name,id,adress,mo_no,state="maharashtra",country="india"):
+    print("yor name is",name,"your ID is:",id,"your mobile number is :",mo_no,"your state is:",state,"your country is;",country)
+    
