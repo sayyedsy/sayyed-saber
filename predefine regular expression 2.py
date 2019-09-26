8010fe2027074ac90b18930cad4b2da428b19126
sayyed saber <55105451+sayyedsy@users.noreply.github.com>
2019-09-26 19:32:39 +0530

    pre defined regular expression practic

---
+++ predefine regular expression 2.py
@@ -0,0 +1,6 @@
+import re
+pattern=re.compile("sayyed")
+match=pattern.finditer("My name is sayyed saber And my mobile number is 8149366131 and my mail id is sayyedsaad80@gmail.com")
+for i in match:
+    print(i.start(),".....",i.end(),"......",i.group())
+    
